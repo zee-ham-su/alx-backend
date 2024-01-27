f3e903a9e8eb6b66e11f1dd1ac6adbea04b7a25e
zee-ham-su <hsufiian@yahoo.com>
2024-01-27 21:57:30 +0000

    del_pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+import csv
+from typing import List, Dict
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """
+        Get hyperpaginated data based on index and page_size.
+        """
+        assert index is None or (isinstance(index, int) and index >= 0)
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.indexed_dataset()
+        total_items = len(dataset)
+
+        if index is not None:
+            assert index < total_items
+
+        start_index = index if index is not None else 0
+        end_index = start_index + page_size
+
+        next_index = end_index if end_index < total_items else None
+        data = [dataset.get(i, []) for i in range(start_index, min(end_index, total_items))]
+
+        return {
+            "index": start_index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data
+        }
