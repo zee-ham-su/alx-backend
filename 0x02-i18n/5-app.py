4b36ca5a846a73b0a42c03f8fbaa315ab564e150
zee-ham-su <hsufiian@yahoo.com>
2024-02-06 20:13:43 +0000

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config(object):
     """Config class"""
     LANGUAGES = ["en", "fr"]
@@ -16,20 +24,13 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 def get_user():
     """ function that returns a user dictionary or
     None if the ID cannot be found or if login_as was not passed."""
-    id_login = request.args.get('login_as')
-    if id_login:
-        return users.get(int(id_login))
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users:
+        return users.get(int(id))
     else:
         return None
 
